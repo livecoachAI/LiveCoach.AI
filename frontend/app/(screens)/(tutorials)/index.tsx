4cddf39f6d51ec846e8364ec6961c877db592ffe
kalana-03 <kalana22malhara22@gmail.com>
2026-01-28 09:34:04 +0530

    Tutorial List is added for cricket and Badminton

--- frontend/app/(screens)/(tutorials)/index.tsx
+++ frontend/app/(screens)/(tutorials)/index.tsx
@@ -1,11 +1,32 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import React, {useState} from 'react';
+import {View, Text, SafeAreaViewBase} from 'react-native';
+import Dropdown from "../../components/Dropdown";
 
-const Index = () => {
-    return (
-        <View>
-            <Text>Index</Text>
-        </View>
-    )
-}
-export default Index
+
+const Tutorial = () => {
+  const [currentStep, setCurrentStep] = useState('cricket');
+
+  const cricketTutorial = [
+    'COVER DRIVE',
+    'LEG GLANCE',
+    'STRAIGHT DRIVE',
+    'SQUARE CUT',
+    'PULL SHOT',
+    'LATE CUT',
+    'PADDLE SWEEP',
+    'SLOG SWEEP',
+  ];
+
+  const bandmintonTutorial = [
+    'SMASH',
+    'CLEAR',
+    'DROP',
+    'NET SHOT'
+  ];
+
+  const techniques = currentStep === 'cricket' ? cricketTutorial : bandmintonTutorial;
+
+
+};
+
+export default Tutorial;
