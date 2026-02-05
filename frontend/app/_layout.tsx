e8ffc15e680a5c96528fce019eec5098976e8d37
Buddhima1030 <buddhimajothiwansa123@gmail.com>
2026-02-05 14:25:22 +0530

    fixing errors in sigin and landing pages

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout() {
     return (
         <Stack screenOptions={{ headerShown: false }}>
             {/* Your route groups are children automatically */}
-            {/* <Stack.Screen name="(auth)" options={{ headerShown: false }} /> */}
-            <Stack.Screen name="(screens)" options={{ headerShown: false }} />
+            {<Stack.Screen name="(auth)" options={{ headerShown: false }} /> }
+            {/* { <Stack.Screen name="(screens)" options={{ headerShown: false }} /> } */}
         </Stack>
     );
 }
