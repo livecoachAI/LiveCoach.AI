59a40da080266e2cd8b3a35b8d0a357910031fbe
Thewan <sewminthewan999@gmail.com>
2026-02-14 18:24:31 +0530

    Move to the analitics page when clicking the analyze button

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,6 +2,6 @@ import { Redirect } from "expo-router";
 
 export default function Index() {
 
-    return <Redirect href="/(screens)/(social)" />;
+    // return <Redirect href="/(screens)/(social)" />;
     return <Redirect href="/(auth)" />;
 }
