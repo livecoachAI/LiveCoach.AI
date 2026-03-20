ec5f4bbfd4541e8f1d55ce4b656d17afad021e2f
Kalana <kalana33471@gmail.com>
2026-03-20 21:35:35 +0530

    testing frontend ci/cd pipeline

--- frontend/app/(screens)/(social)/social.tsx
+++ frontend/app/(screens)/(social)/social.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const Social = () => {
     return (
         <View>
-            
+            <Text>Coming Soon</Text>
         </View>
     )
 }
